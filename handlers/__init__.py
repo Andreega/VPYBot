7115e060e303b44e62e417dd078e8133cad53329
mykyttem <nrabicenko5@gmail.com>
2024-04-18 10:25:07 +0300

    aded

---
+++ handlers/__init__.py
@@ -0,0 +1,7 @@
+from .command_handlers import *
+from .message_handlers import *
+from .conversation_handlers import *
+
+from .base_handler import BaseHandler
+
+
---
+++ handlers/command_handlers/__init__.py
@@ -0,0 +1,6 @@
+
+from .hello_handler import HelloHandler
+from .start_handler import StartHandler
+
+
+from handlers.base_handler import BaseHandler
